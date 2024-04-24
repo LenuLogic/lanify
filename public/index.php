7922f659066c9074510e792799b6ac31a02171eb
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-04-24 16:01:03 +0300

    Luodaan uusi Plates-olio ja kytketään sivupohjat osaksi sovellusta index.php:hen.

--- public/index.php
+++ public/index.php
@@ -6,9 +6,22 @@ require_once '../src/init.php';
 $request=str_replace($config['urls']['baseUrl'],'',$SERVER['REQUEST_URI']);
 $request=strtok($request,'?');
 
+//Luodaan uusi Plates-olio ja kytketään se sivupohjaan:
+$templates = new League\Plates\Engine('../src/view');
 
-/* Selvitetään, mitä sivua on kutsuttu, ja suoritetaan 
-sivua vastaava käsittelijä (eli PHP-koodi). */
+//Selvitetään kutsuttu sivu ja suoritetaan sitä vastaava käsittelijä:
+if ($request === '/' || $request === '/tapahtumat') {
+    echo $templates->render('/tapahtumat'); 
+} else if ($request === '/tapahtuma') {
+    echo $templates->render('tapahtuma');
+} else {
+    echo $templates->render('notfound');
+}
+
+?>
+
+<!-- Selvitetään, mitä sivua on kutsuttu, ja suoritetaan 
+sivua vastaava käsittelijä (eli PHP-koodi). 
 if ($request==="/" || $request === "/tapahtumat") {
     echo "<h1>Kaikki tapahtumat</h1>";
 } else if ($request === "/tapahtuma") {
@@ -16,4 +29,5 @@ if ($request==="/" || $request === "/tapahtumat") {
 } else {
     echo "<h1>Pyydettyä sivua ei löytynyt :}</h1>";
 }
-?>
+Tämä olikin apupyöräskripti.
+-->
