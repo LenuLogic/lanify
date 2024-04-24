89ffbeb863e5de51f4412b6ac1b7c9987a593710
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-04-24 15:01:46 +0300

    Platesin asennus ja template.php-sivupohjan luominen

--- src/init.php
+++ src/init.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../config/config.php';
+require_once '../vendor/autoload.php';
 ?>
 
 <!-- Mihin tätä erillistä tiedostoa tarvitaan? 
---
+++ src/view/template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <title>Lanify - <?=$this->e($title)?></title>
+</head>
+<body>
+    <header>
+        <h1>Lanify</h1>
+    </header>
+    <section>
+        <?=$this->section('content')?>
+    </section>
+<footer>
+    <hr>
+    <div>Lanify by Lenu Logic</div>
+</footer>
+</body>
+</html>
