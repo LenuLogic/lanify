9a1bca97e5be8b6bf0cc9e2c4ce04ec92583d756
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-04-24 15:12:49 +0300

    Luodaan pohja tapahtuma-, tapahtumat- ja virhesivuille.

---
+++ src/view/tapahtuma.php
@@ -0,0 +1,5 @@
+<?php
+$this->layout('template', ['title' => 'Tapahtuma'])
+?>
+
+<h1>Tapahtuma</h1>
---
+++ src/view/tapahtumat.php
@@ -0,0 +1,5 @@
+<?php
+$this->layout('template', ['title' => 'Tulevat tapahtumat'])
+?>
+
+<h1>Tulevat tapahtumat</h1>
