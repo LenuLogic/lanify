b104246fe4dbb35abd020799641ff78d5b96c021
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-04-25 13:55:37 +0300

    Korjattu php-tiedostojen kommentteja - tyhjä sivu hävisi.

--- public/index.php
+++ public/index.php
@@ -3,15 +3,15 @@
 require_once '../src/init.php';
 
 /*Tämä siistii urlin lyhemmäksi, ettei tuu järkkypitkää.*/ 
-$request=str_replace($config['urls']['baseUrl'],'',$SERVER['REQUEST_URI']);
+$request=str_replace($config['urls']['baseUrl'],'',$_SERVER['REQUEST_URI']);
 $request=strtok($request,'?');
 
 //Luodaan uusi Plates-olio ja kytketään se sivupohjaan:
-$templates = new League\Plates\Engine('../src/view');
+$templates = new League\Plates\Engine('../src/view'); // Kokeilen vaihtaa kenoviivat - ei
 
 //Selvitetään kutsuttu sivu ja suoritetaan sitä vastaava käsittelijä:
 if ($request === '/' || $request === '/tapahtumat') {
-    echo $templates->render('/tapahtumat'); 
+    echo $templates->render('tapahtumat'); 
 } else if ($request === '/tapahtuma') {
     echo $templates->render('tapahtuma');
 } else {
@@ -19,15 +19,3 @@ if ($request === '/' || $request === '/tapahtumat') {
 }
 
 ?>
-
-<!-- Selvitetään, mitä sivua on kutsuttu, ja suoritetaan 
-sivua vastaava käsittelijä (eli PHP-koodi). 
-if ($request==="/" || $request === "/tapahtumat") {
-    echo "<h1>Kaikki tapahtumat</h1>";
-} else if ($request === "/tapahtuma") {
-    echo "<h1>Yksittäisen tapahtuman tiedot</h1>";
-} else {
-    echo "<h1>Pyydettyä sivua ei löytynyt :}</h1>";
-}
-Tämä olikin apupyöräskripti.
--->
--- src/init.php
+++ src/init.php
@@ -2,6 +2,3 @@
 require_once '../config/config.php';
 require_once '../vendor/autoload.php';
 ?>
-
-<!-- Mihin tätä erillistä tiedostoa tarvitaan? 
-Miksei require_once-komentoa vaan kirjoiteta skriptin alkuun?*/
