ad2975042fe682820c182ccaa23af754e33dd546
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-04-29 11:11:30 +0300

    Yksittäisen tapahtuman tietojen tulostaminen

--- src/model/tapahtuma.php
+++ src/model/tapahtuma.php
@@ -6,4 +6,7 @@ function haeTapahtumat() {
     return DB::run('SELECT*FROM tapahtuma ORDER BY tap_alkaa;')->fetchAll();
     }
 
+function haeTapahtuma($id) {
+    return DB::run('SELECT * FROM tapahtuma WHERE idtapahtuma = ?;',[$id])->fetch();
+}
 ?>
