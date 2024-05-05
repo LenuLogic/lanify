36152c80f33215789298855de6ecf8c618615d6c
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-05-05 11:58:43 +0300

    Henkilön lisääminen tietokantaan

---
+++ src/model/henkilo.php
@@ -0,0 +1,9 @@
+<?php
+//Suoritetaan ensin helpers-kansiossa oleva DB.php-tiedosto.
+require_once HELPERS_DIR . 'DB.php';
+
+function lisaaHenkilo($nimi,$email,$discord,$salasana) {
+    DB::run('INSERT INTO henkilo (nimi, email, discord, salasana) VALUE (?,?,?,?);',[$nimi,$email,$discord,$salasana]);
+    return DB::lastInsertId();
+}
+?>
