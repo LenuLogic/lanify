5a2a7a28cadd4752d5e17a91c71b087663144056
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-05-10 15:27:08 +0300

    Lisätään uloskirjautuminen.

--- src/controller/kirjaudu.php
+++ src/controller/kirjaudu.php
@@ -13,4 +13,17 @@ function tarkistaKirjautuminen($email="", $salasana="") {
     }
     return false;
 }
+
+function logout() {
+    $_SESSION = array(); // Tyhjentää istuntomuuttujat.
+
+    if (ini_get("session.use_cookies")) { // Poistaa istunnon evästeen.
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+    );
+    }
+    session_destroy(); // Tuhoaa istunnon.
+}
 ?>
